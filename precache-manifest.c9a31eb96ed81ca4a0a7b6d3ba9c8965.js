ac98e0520e55cfaf20826acd7b26c20bac40e4cc
narender bisht <narender007bisht@gmail.com>
2020-11-15 03:45:47 -0800

    Updates

---
+++ precache-manifest.c9a31eb96ed81ca4a0a7b6d3ba9c8965.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8234f2fd08086ddbb7a758e491c8d75a",
+    "url": "/addusers-live/index.html"
+  },
+  {
+    "revision": "61ce19c8c6615c2fd641",
+    "url": "/addusers-live/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "55534cc3f7d656831f70",
+    "url": "/addusers-live/static/css/main.932c2beb.chunk.css"
+  },
+  {
+    "revision": "61ce19c8c6615c2fd641",
+    "url": "/addusers-live/static/js/2.372b05ee.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/addusers-live/static/js/2.372b05ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55534cc3f7d656831f70",
+    "url": "/addusers-live/static/js/main.2d18080e.chunk.js"
+  },
+  {
+    "revision": "ca018d72bbcfdc5cc724",
+    "url": "/addusers-live/static/js/runtime-main.6776647b.js"
+  }
+]);
